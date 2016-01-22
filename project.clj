d3256e61884af09735d21ec5adefe7c662a0a748
David Blurton <david.blurton@gmail.com>
2016-01-22 20:37:40 +0000

    Start a web server that returns sample data

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-json "0.4.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
-  :plugins [[speclj "3.3.1"]]
+  :plugins [[speclj "3.3.1"]
+            [lein-ring "0.7.1"]]
   :test-paths ["spec"]
-  :main "tala.core")
+  :ring {:handler tala.core/app})
--- src/tala/core.clj
+++ src/tala/core.clj
@@ -1,6 +1,10 @@
 (ns tala.core
+  (:use compojure.core)
   (:require [tala.import]
-            [tala.parse-grammar-tags :as parse])
+            [tala.parse-grammar-tags :as parse]
+            [ring.middleware.json :as middleware]
+            [compojure.route :as route]
+            [compojure.handler :as handler])
   (:use [clojure.pprint :only (pprint)]))
 
 (defn mapTags [form]
@@ -9,11 +13,17 @@
 (defn mapForms [forms]
   (map #(assoc % :tags (mapTags %)) forms))
 
-(defn -main
-  "Start the tala api server"
-  []
-  (def data (tala.import/read-csv "resources/sample.csv"))
-  (def words (tala.import/parse-data data))
-  (def forms (mapForms (:forms words)))
+(def data (tala.import/read-csv "resources/sample.csv"))
+(def words (tala.import/parse-data data))
+(def forms (mapForms (:forms words)))
+(def result (assoc-in words [:forms] forms))
 
-  (pprint (assoc-in words [:forms] forms)))
+(defroutes app-routes
+  (GET "/" [] {:body result})
+  (route/not-found "<h1>Page not found</h1>"))
+
+(def app
+  (-> (handler/api app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-params)
+      (middleware/wrap-json-response)))
