d5f272dabe7fd53355fb563a68ff098f3fbf667c
David Blurton <david.blurton@gmail.com>
2016-01-23 16:45:10 +0000

    Run as a servlet for google app engine

--- project.clj
+++ project.clj
@@ -2,12 +2,17 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
+                 [ring-server "0.4.0"]
+                 [ring/ring-core "1.4.0"]
                  [ring/ring-json "0.4.0"]
+                 [ring/ring-servlet "1.1.0"]
                  [amalloy/ring-gzip-middleware "0.1.3"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
+  :aot [tala.core]
+  :profiles {:dev        {:dependencies [[speclj "3.3.1"]]}
+             :production {:ring {:open-browser? false, :stacktraces? true, :auto-reload? false}}}
   :plugins [[speclj "3.3.1"]
             [lein-ring "0.7.1"]]
   :test-paths ["spec"]
--- src/tala/core.clj
+++ src/tala/core.clj
@@ -1,12 +1,14 @@
 (ns tala.core
+  (:gen-class :extends javax.servlet.http.HttpServlet)
   (:use compojure.core)
   (:require [tala.import]
             [tala.parse-grammar-tags :as parse]
             [ring.middleware.json :as middleware]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [ring.middleware.gzip])
-  (:use [clojure.pprint :only (pprint)]))
+            [ring.util.servlet :as servlet]
+            [ring.middleware.gzip]
+            [clojure.java.io :as io]))
 
 (defn mapTags [form]
   (parse/parse-tag (:grammar-tag form)))
@@ -14,7 +16,7 @@
 (defn mapForms [forms]
   (map #(assoc % :tags (mapTags %)) forms))
 
-(def data (tala.import/read-csv "resources/sample.csv"))
+(def data (tala.import/read-csv (io/resource "sample.csv")))
 (def words (tala.import/parse-data data))
 (def forms (mapForms (:forms words)))
 (def result (assoc-in words [:forms] forms))
@@ -29,3 +31,5 @@
       (middleware/wrap-json-params)
       (middleware/wrap-json-response)
       (ring.middleware.gzip/wrap-gzip)))
+
+(servlet/defservice app)
